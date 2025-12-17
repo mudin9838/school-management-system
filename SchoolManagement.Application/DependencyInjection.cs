cd2f616a21f39736d76b68b54baec44663034b77
Muhdin <70258285+mudin9838@users.noreply.github.com>
2025-12-17 21:34:56 +0300

    feat: add application layer with cqrs query for students

---
+++ SchoolManagement.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SchoolManagement.Application;
+
+public static class DependencyInjection
+{
+    /// <summary>
+    /// This code snippet is a static method named AddApplicationServices that extends the IServiceCollection interface. It adds MediatR services to the services collection by registering services from the assembly that contains the DependencyInjection class. The method then returns the updated services collection.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+        return services;
+    }
+}
