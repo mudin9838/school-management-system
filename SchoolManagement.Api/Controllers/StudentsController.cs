82081854ab3e8993f62094329ca60a8f86a2b9c5
Muhdin <70258285+mudin9838@users.noreply.github.com>
2025-12-18 15:11:36 +0300

    feat: add students api controller with cqrs query

---
+++ SchoolManagement.Api/Controllers/StudentsController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.Application.Features.Students.Queries;
+
+namespace SchoolManagement.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StudentsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public StudentsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(await _mediator.Send(new GetAllStudentsQuery()));
+    }
+}
