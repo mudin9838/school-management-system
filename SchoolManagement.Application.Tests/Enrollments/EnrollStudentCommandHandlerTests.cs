9f6dbf9d2746778c78375c55259ec15ee1f3b53d
Muhdin <70258285+mudin9838@users.noreply.github.com>
2025-12-19 22:36:11 +0300

    test: add unit tests for enrollment business rules

---
+++ SchoolManagement.Application.Tests/Enrollments/EnrollStudentCommandHandlerTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Moq;
+using SchoolManagement.Application.Exceptions;
+using SchoolManagement.Application.Features.Enrollments.Commands.EnrollStudent;
+using SchoolManagement.Application.Interfaces;
+using SchoolManagement.Domain.Entities;
+
+namespace SchoolManagement.Application.Tests.Enrollments;
+
+public class EnrollStudentCommandHandlerTests
+{
+    private readonly Mock<IEnrollmentRepository> _repoMock;
+    private readonly EnrollStudentCommandHandler _handler;
+
+    public EnrollStudentCommandHandlerTests()
+    {
+        _repoMock = new Mock<IEnrollmentRepository>();
+        _handler = new EnrollStudentCommandHandler(_repoMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrow_WhenStudentAlreadyEnrolled()
+    {
+        //Arrange
+        var studentId = Guid.NewGuid();
+        var classId = Guid.NewGuid();
+
+        _repoMock.Setup(r => r.ExistsAsync(studentId, classId))
+            .ReturnsAsync(true);
+        var command = new EnrollStudentCommand(studentId, classId);
+
+        //Act
+        var act = async () => await _handler.Handle(command, default);
+
+        //Assert
+        await act.Should()
+            .ThrowAsync<BusinessRuleViolationException>()
+            .WithMessage("Student is already enrolled in this class");
+    }
+
+
+    [Fact]
+    public async Task Handle_ShouldEnroll_WhenNotAlreadyEnrolled()
+    {
+        //Arrange
+        var studentId = Guid.NewGuid();
+        var classId = Guid.NewGuid();
+
+        _repoMock.Setup(r => r.ExistsAsync(studentId, classId))
+            .ReturnsAsync(false);
+        var command = new EnrollStudentCommand(studentId, classId);
+
+        //Act
+        await _handler.Handle(command, default);
+
+        //Assert
+        _repoMock.Verify(
+            r => r.AddAsync(It.IsAny<Enrollment>()),
+            Times.Once);//  AddAsync method of the _repoMock object is called exactly once with any Enrollment object.
+    }
+}
